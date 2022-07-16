6eff96989978439c904826dbb0e0364e16554d7b
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-07-16 13:40:01 +0200

    Update filees

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const form = document.querySelector('.js-form');
 const recipesContainer = document.querySelector('.js-recipes');
+const searchInput = document.querySelector('.js-input');
+const errorMessage = document.querySelector('.js-error');
+
 
 function submitForm(e) {
     e.preventDefault();
+    const value = searchInput.value;
+
+    if (value.length > 0) {
+        errorMessage.innerHTML = '';
+    } else {
+        errorMessage.innerHTML = `<p id="error_msg">Invalid search: empty input</p>`;
+    }
 }
 
+form.addEventListener('submit', submitForm);
