1165b412cbbfe14060e07cdc5c25c8337c65e24f
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-07-16 13:47:29 +0200

    JS changes, API settings

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-const form = document.querySelector('.js-form');
+const searchForm = document.querySelector('.js-form');
 const recipesContainer = document.querySelector('.js-recipes');
 const searchInput = document.querySelector('.js-input');
 const errorMessage = document.querySelector('.js-error');
 
+const API_URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=`;
+
+const getApiUrl = search => API_URL + search;
 
 function submitForm(e) {
     e.preventDefault();
-    const value = searchInput.value;
+    const value = searchInput.value.trim();
 
     if (value.length > 0) {
         errorMessage.innerHTML = '';
+        fetch(getApiUrl(value))
+            .then(data => data.json())
+
     } else {
-        errorMessage.innerHTML = `<p id="error_msg">Invalid search: empty input</p>`;
+        errorMessage.innerHTML = 
+        `<p id="error_msg">Invalid search: empty input</p>`;
     }
 }
 
-form.addEventListener('submit', submitForm);
+searchForm.addEventListener('submit', submitForm);
