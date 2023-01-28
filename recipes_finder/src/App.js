cd1554e07edef9088ebaad6f44d2a514d4e1ac55
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-01-28 12:20:24 +0100

    Import RecipeFinder

--- recipes_finder/src/App.js
+++ recipes_finder/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { RecipeFinder } from './Components/RecipeFinder';
 
 function App() {
   return (
     <div className="App">
-
+      <RecipeFinder />
     </div>
   );
 }
