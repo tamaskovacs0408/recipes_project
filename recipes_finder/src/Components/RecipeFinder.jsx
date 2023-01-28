a3b06ce04da14a39253987c3bc856add27000223
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-01-28 12:55:00 +0100

    Update component

--- recipes_finder/src/Components/RecipeFinder.jsx
+++ recipes_finder/src/Components/RecipeFinder.jsx
@@ -2,20 +2,20 @@ import React, { useState } from "react";
 
 export const RecipeFinder = () => {
   const [searchValue, setSearchValue] = useState(null);
-  const [recipes, setRecipes] = useState(null);
+  const [recipes, setRecipes] = useState("");
   const [searchError, setSearchError] = useState(null);
 
 async function handleSubmit(e) {
   e.preventDefault();
-
   await fetch(
     `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`
   )
     .then(async (response) => await response.json())
-    .then(async (recipeData) => setRecipes(recipeData[1]));
-
-    console.log(recipes)
+    .then(async (data) => setRecipes(data.meals));
+    
 }
+
+console.log(recipes);
 
   return (
     <div className="form__container">
