cc1bd0ebb9c10de971b9c0fdedf9a193063cca0e
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-01-28 15:51:14 +0100

    Create array for ingredients

--- recipes_finder/src/Components/Recipe/Recipe.jsx
+++ recipes_finder/src/Components/Recipe/Recipe.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import './index.scss'
+import React from "react";
+import "./index.scss";
 
-export const Recipe = ({data}) => {
+export const Recipe = ({ data }) => {
+  let ingredients = [];
+  for (let i = 1; i <= 20; i++) {
+    let value = data[`strIngredient${i}`];
+    if (typeof value === "string" && value.length > 0) {
+      ingredients.push(value)
+    }
+  }
+  console.log(data)
+  console.log(ingredients)
   return (
     <>
       <div>
         <h2>{data.strMeal}</h2>
         <p>{data.strInstructions}</p>
+        <div className="ingredients">
+          <h3>Ingredients:</h3>
+        </div>
+        <ul>
+          {ingredients.map(element => {
+            return <li key={data.strMeal}>{element}</li>
+          })}
+        </ul>
       </div>
     </>
   );
-}
+};
