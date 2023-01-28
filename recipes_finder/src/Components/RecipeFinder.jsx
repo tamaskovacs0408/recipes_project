924ae7d686355a3f6106ce0d5d7e3a49573a4815
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-01-28 12:04:15 +0100

    Update component

--- recipes_finder/src/Components/RecipeFinder.jsx
+++ recipes_finder/src/Components/RecipeFinder.jsx
@@ -1,9 +1,12 @@
 import React, {useState, useEffect} from 'react';
+import '.index.scss';
 
 export const RecipeFinder = () => {
   const API_URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=`;
+  const [searchValue, setSearchValue] = useState(null);
+  const [recipes, setRecipes] = useState(null);
 
-
+  
 
   return (
     <div className='form__container'>
