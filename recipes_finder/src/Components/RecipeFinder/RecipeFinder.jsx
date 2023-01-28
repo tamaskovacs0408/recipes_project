16debc56134ee6525512c3b8909a5e3d0b46cfee
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-01-28 14:49:37 +0100

    Update data fetching

---
+++ recipes_finder/src/Components/RecipeFinder/RecipeFinder.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useRef } from "react";
+import "./index.scss";
+
+export const RecipeFinder = () => {
+  const [recipes, setRecipes] = useState("");
+  const [searchError, setSearchError] = useState(null);
+  const inputRef = useRef();
+
+async function handleSubmit(e) {
+  e.preventDefault();
+  let searchRef = inputRef.current.value;
+
+  searchRef = searchRef.trim();
+
+  if (searchRef === null || searchRef === '') {
+    setSearchError("Invalid value")
+  } else {
+    await fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchRef}`
+    )
+      .then(async (response) => await response.json())
+      .then(async (data) => setRecipes(data.meals));
+  }
+}
+
+console.log(recipes);
+
+  return (
+    <div className="form__container">
+      <form className="form" onSubmit={handleSubmit}>
+        <label className="form__label">
+          <input
+            type="text"
+            name="food"
+            className="form__label--input"
+            ref={inputRef}
+          />
+        </label>
+        <button type="submit" className="form__button">
+          Search
+        </button>
+        <p className="form__error">{searchError}</p>
+      </form>
+    </div>
+  );
+};
