2c31f29b7aeeca4470cb4af153100803bf729b82
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-01-30 18:24:37 +0100

    Set up modal for recipes

--- recipes_finder/src/Components/RecipeModal/RecipeModal.jsx
+++ recipes_finder/src/Components/RecipeModal/RecipeModal.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import uuid from "react-uuid";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
+import "./index.scss";
 
 export const RecipeModal = ({ data, open, close }) => {
   let ingredients = [];
@@ -25,53 +23,28 @@ export const RecipeModal = ({ data, open, close }) => {
     return acc;
   }, {});
 
-  const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
-
   return (
-    <div className="modal" aria-hidden="true">
-      <Modal
-        open={open}
-        onClose={close}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
-      {/* <div className="modal-content">
-        <h2>{data.strMeal}</h2>
-        <img src={data.strMealThumb} alt={data.strMeal} />
-        <p>{data.strInstructions}</p>
-        <p>{data.strInstructions}</p>
-        <div className="ingredients">
-          <h3>Ingredients:</h3>
-        </div>
-        <ul>
-          {Object.entries(ingObj).map(([ingredient, measure]) => {
-            return (
-              <li key={uuid()}>
-                {ingredient}: {measure}
-              </li>
-            );
-          })}
-        </ul>
-      </div> */}
-    </div>
+    <Modal
+      open={open}
+      onClose={close}
+    >
+      <div className="modal">
+          <h2>{data.strMeal}</h2>
+          <img src={data.strMealThumb} alt={data.strMeal} />
+          <p>{data.strInstructions}</p>
+          <div className="ingredients">
+            <h3>Ingredients:</h3>
+          </div>
+          <ul>
+            {Object.entries(ingObj).map(([ingredient, measure]) => {
+              return (
+                <li key={uuid()}>
+                  {ingredient}: {measure}
+                </li>
+              );
+            })}
+          </ul>
+      </div>
+    </Modal>
   );
 };
