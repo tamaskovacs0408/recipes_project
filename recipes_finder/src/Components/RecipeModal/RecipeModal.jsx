d30649a5c4896d952e646fd7860a20c8a551807b
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-01-30 17:36:08 +0100

    Set up modal

--- recipes_finder/src/Components/RecipeModal/RecipeModal.jsx
+++ recipes_finder/src/Components/RecipeModal/RecipeModal.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import uuid from "react-uuid";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
-export const RecipeModal = ({ data }) => {
+export const RecipeModal = ({ data, open, close }) => {
   let ingredients = [];
   let measures = [];
   for (let i = 1; i <= 20; i++) {
@@ -21,9 +25,36 @@ export const RecipeModal = ({ data }) => {
     return acc;
   }, {});
 
+  const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
   return (
     <div className="modal" aria-hidden="true">
-      <div className="modal-content">
+      <Modal
+        open={open}
+        onClose={close}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+      {/* <div className="modal-content">
         <h2>{data.strMeal}</h2>
         <img src={data.strMealThumb} alt={data.strMeal} />
         <p>{data.strInstructions}</p>
@@ -40,7 +71,7 @@ export const RecipeModal = ({ data }) => {
             );
           })}
         </ul>
-      </div>
+      </div> */}
     </div>
   );
 };
