f2931ac3f35411509b67b8cb1476f24db556facf
Tamás Kovács <kovacs.tamas0408@gmail.com>
2023-02-03 09:59:46 +0100

    Update component

--- recipes_finder/src/App.js
+++ recipes_finder/src/App.js
@@ -4,6 +4,9 @@ import {RecipeFinder} from "./Components/RecipeFinder/RecipeFinder";
 function App() {
   return (
     <div className="App">
+      <header>
+        <h1>Recipes from around the world</h1>
+      </header>
       <RecipeFinder />
     </div>
   );
